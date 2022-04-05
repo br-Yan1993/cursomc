acda5e1165d1f8d2cad12ec9943fd501f6d12a24
Yan Carlos <br_yan993@hotmail.com>
2022-04-04 22:22:10 -0300

    Listando Todas as Categorias com DTO

--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.example.demo.services;
 
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,4 +46,9 @@ public class CategoriaService {
 					+ "ligados a ela.");
 		}
 	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+
 }
